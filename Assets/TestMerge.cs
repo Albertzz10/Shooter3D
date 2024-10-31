0cda069677e4ed7fbf4575ba5765d0e3531db25f
Albertzz10 <clarineteaaz@gmail.com>
2024-10-31 11:19:12 +0100

    Update TestMerge.cs

--- Assets/TestMerge.cs
+++ Assets/TestMerge.cs
@@ -5,7 +5,7 @@ public class TestMerge : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("prueba Rama");
+        //Debug.Log("prueba Rama");
     }
 
     // Update is called once per frame
